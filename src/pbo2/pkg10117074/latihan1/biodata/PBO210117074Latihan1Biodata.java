9d631139132399c8e858ae01637368ef0eaba6c1
MY PC <MY PC@HOME-UEFI1TB.local>
2018-09-23 03:57:30 +0700

    Project Brigita Julia 10117074 IF2

---
+++ src/pbo2/pkg10117074/latihan1/biodata/PBO210117074Latihan1Biodata.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117074.latihan1.biodata;
+
+/**
+ *
+ * @author Rizki Adam Kurniawan
+ */
+public class PBO210117074Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Biodata" + "\n" + "--------------------------");
+        System.out.println("Nama\t\t: Brigita Julia Puspita Nai Goncalves");
+        System.out.println("Alamat\t\t: Jl. Bagusrangin No.04");
+        System.out.println("Tempat Lahir\t: Dili");
+        System.out.println("Tanggal lahir\t: 15 Januari 1998");
+        System.out.println("-----------------------------------");
+        System.out.println("Hobi\t\t: Dengerin lagu, ngedrakor");
+        System.out.println("MaKes\t\t: Masakan mama, empek-empek");
+        System.out.println("MiKes\t\t: Teh kotak");
+    }
+    
+}
